3f2d1b2041dc1ab2875caf26331ffa9c485caeaa
Jussania <https://github.com/Jussania/deafio-poo-dio.git>
2022-08-03 10:30:44 -0300

    Adicionado idade ao dev

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class Dev {
     private String nome;
+    private int idade;
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
@@ -44,6 +45,14 @@ public class Dev {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
     }
 
     public Set<Conteudo> getConteudosInscritos() {
