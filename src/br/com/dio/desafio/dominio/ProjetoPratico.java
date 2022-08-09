639791cf9d29cf760bde0a19ff765f599383b159
hanakathy <khanamartins@gmail.com>
2022-08-09 19:14:08 -0400

    criou classe ProjetoPratico

---
+++ src/br/com/dio/desafio/dominio/ProjetoPratico.java
@@ -0,0 +1,36 @@
+package br.com.dio.desafio.dominio;
+
+//Classe de Projeto onde o Dev pode aprimorar o aprendizado 
+public class ProjetoPratico extends Conteudo {
+	
+	private int nivelDificuldade;
+	
+	@Override
+	public double calcularXp() {
+		return XP_PADRAO * nivelDificuldade;
+	}
+
+	public int getNivelDificuldade() {
+		return nivelDificuldade;
+	}
+
+	public void setNivelDificuldade(int nivelDificuldade) {
+		this.nivelDificuldade = nivelDificuldade;
+	}
+
+	@Override
+	public String toString() {
+		return "Projeto Pratico " + getTitulo()
+				+ ", Descrição" + getDescricao()
+				+ "Nivel de Dificuldade: " + nivelDificuldade;
+	}
+
+	public ProjetoPratico() {
+		
+	}	
+	
+	
+	
+	//CTRL + SHIFT + L -> Atalhos pra ver os atalhos no eclipse
+	
+}
